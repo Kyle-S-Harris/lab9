962165843273570978d4ba2b5bddc4fa28a8b1ca
Kyle Stephan Harris <kharris@ucsd.edu>
2020-03-06 00:20:52 -0800

    Added Debugging 101 info back to code and attempted to make it display properly.

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,18 @@
 var labs = require('../labs.json');
 
 exports.view = function(req, res){
-  res.render('index', {
-	'labs': labs
-  });
+  if(labs.length < 9) {
+  	var todaysLab = [{
+		"id": "lab-9",
+		"title": "Debugging 101",
+		"date": "March 7",
+		"image": "debug.png"
+	}];
+
+    labs.push(todaysLab);
+    res.render('index', {
+      'labs': labs, todaysLab
+      });
+  }
+  
 };
